75b60b3ef8ed81ebf427ebebffdaa849c4e692cf
reiwa <reiwa@outlook.com.vn>
2021-01-13 00:13:56 +0700

    update: エラーを発生させる

--- app/users/queries/getCurrentUser.ts
+++ app/users/queries/getCurrentUser.ts
@@ -1,13 +1,16 @@
-import { Ctx } from "blitz"
+import { AuthorizationError, Ctx } from "blitz"
 import db from "db"
 
-export default async function getCurrentUser(_ = null, { session }: Ctx) {
-  if (!session.userId) return null
+export default async function getCurrentUser(_ = null, ctx: Ctx) {
+  ctx.session.authorize()
 
   const user = await db.user.findFirst({
-    where: { id: session.userId },
-    select: { id: true, name: true, email: true, role: true },
+    where: { id: ctx.session.userId },
   })
+
+  if (user === null) {
+    throw new AuthorizationError()
+  }
 
   return user
 }
