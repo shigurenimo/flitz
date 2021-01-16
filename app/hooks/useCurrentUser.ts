6f3319c1a8d1836b81e51a234e2e834370b42261
reiwa <reiwa@outlook.com.vn>
2021-01-17 06:22:52 +0700

    refactor: 不要な関数を削除する

--- app/hooks/useCurrentUser.ts
+++
@@ -1,8 +0,0 @@
-import getCurrentUser from "app/users/queries/getCurrentUser"
-import { useQuery } from "blitz"
-
-export const useCurrentUser = () => {
-  const [user] = useQuery(getCurrentUser, null)
-
-  return user
-}
