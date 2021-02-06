928e26eb4f3c25befa72591ff134d5c148b6f064
reiwa <reiwa@outlook.com.vn>
2021-02-07 03:13:45 +0700

    fix: withPWAが動作しないので外す

--- blitz.config.js
+++ blitz.config.js
@@ -2,13 +2,8 @@ const {
   sessionMiddleware,
   simpleRolesIsAuthorized,
 } = require("@blitzjs/server")
-const withPWA = require("next-pwa")
 
-module.exports = withPWA({
+module.exports = {
   log: { level: "error" },
   middleware: [sessionMiddleware({ isAuthorized: simpleRolesIsAuthorized })],
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
-})
+}
